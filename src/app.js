2e4f280495c80ad5f52c66c1525b9a83809b9b26
Pas0ta <ssantamariarodrigo@gmail.com>
2022-12-10 11:19:12 +0000

    version 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let a = ["Hello Rigo from the console!"];
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * what.length);
+  let rdm3 = Math.floor(Math.random() * when.length);
+  document.write.body.innerHTML = a + who[rdm1] + what[rdm2] + when[rdm3];
 };
